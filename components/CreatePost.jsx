b39f56298ad83b2685a6c22b17c597a0a786180c
7sumona02 <sumona.23548@sscbs.du.ac.in>
2025-11-06 16:56:48 +0530

    added

--- components/CreatePost.jsx
+++ components/CreatePost.jsx
@@ -8,32 +8,32 @@ import { Calendar, ImageIcon, MapPin, SmileIcon, X } from 'lucide-react'
 import { useRef, useState } from 'react'
 
 const CreatePost = () => {
-    const [post,setPost] = useState('')
+    const [post, setPost] = useState('')
     const isDisabled = post.trim() === ""
     const fileRef = useRef(null)
 
     const [imagePreview, setImagePreview] = useState(null)
 
-     const handleImageClick = () => {
-    fileRef.current?.click()
-  }
-
-  const handleFileChange = (e) => {
-    const file = e.target.files[0]
-    if (file) {
-      setImagePreview(URL.createObjectURL(file))
+    const handleImageClick = () => {
+        fileRef.current?.click()
     }
-  }
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0]
+        if (file) {
+            setImagePreview(URL.createObjectURL(file))
+        }
+    }
     return (
         <div className="flex flex-col items- justify-start mt-5 border-b pb-5 w-[55.4vw] pl-20 pr-5 space-y-5">
             <div className="w-full flex gap-1 ">
                 <div>
-                <Avatar className='size-10'>
-                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                    <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-            </div>
-            <div><Textarea value={post} onChange={(e) => setPost(e.target.value)} className='border-none shadow-none w-[45vw] focus:outline-none focus:ring-0' placeholder='What is happening?' /></div>
+                    <Avatar className='size-10'>
+                        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                </div>
+                <div><Textarea value={post} onChange={(e) => setPost(e.target.value)} className='border-none shadow-none w-[45vw] focus:outline-none focus:ring-0' placeholder='What is happening?' /></div>
             </div>
             {imagePreview && <div className='w-full h-[50vh] rounded-md overflow-hidden relative'>
                 <img src={imagePreview} className='w-full h-full object-cover' />
@@ -41,7 +41,7 @@ const CreatePost = () => {
             </div>}
             <div className='flex justify-between items-center'>
                 <div className='flex gap-3 items-center'>
-                    <Input type='file' ref={fileRef} className='hidden' onChange={handleFileChange} /><ImageIcon size={20} className='cursor-pointer' onClick={handleImageClick}  />
+                    <Input type='file' ref={fileRef} className='hidden' onChange={handleFileChange} /><ImageIcon size={20} className='cursor-pointer' onClick={handleImageClick} />
                     <div><SmileIcon size={20} className='cursor-pointer' /></div>
                     <div><MapPin size={20} className='cursor-pointer' /></div>
                     <div><Calendar size={20} className='cursor-pointer' /></div>
