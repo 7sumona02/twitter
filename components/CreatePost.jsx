2fbfc94c7c34a43b84bcaf937c20009fedd45f88
7sumona02 <sumona.23548@sscbs.du.ac.in>
2025-11-07 15:47:02 +0530

    pending state

--- components/CreatePost.jsx
+++ components/CreatePost.jsx
@@ -61,7 +61,7 @@ const CreatePost = () => {
     if (!session) return null
     if (!profile) return null
     if (loading) return (
-        <div className="flex flex-col space-y-3">
+        <div className={`flex flex-col space-y-3 ${isPending ? 'opacity-50' : 'opacity-100'}`}>
             <Skeleton className="h-[125px] w-[250px] rounded-xl" />
             <div className="space-y-2">
                 <Skeleton className="h-4 w-[250px]" />
